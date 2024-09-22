e57e759e2bc58787455ed2b25304ee905506909a
Abhinand-R <abhinand62003@gmail.com>
2024-09-23 03:07:29 +0530

    script-download-contactform

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// timeline.js
 function openFullscreen(imgSrc) {
     const fullscreenImg = document.getElementById('fullscreenImg');
     const fullscreenContainer = document.getElementById('fullscreenImage');
@@ -18,4 +17,10 @@ document.getElementById('fullscreenImage').addEventListener('click', function(e)
     if (e.target === this) {
         closeFullscreen();
     }
+});
+
+document.querySelector('.download-section .btn').addEventListener('click', function(e) {
+    // If you want to perform any actions before or after the download, you can do so here
+    console.log('Download initiated');
+    // The actual download will be handled by the browser due to the 'download' attribute
 });
